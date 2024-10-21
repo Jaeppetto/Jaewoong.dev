9bb1c1a222f6bf43b0ca985e334f20c7ee0128c1
Jaewoong Hwang <noa9925@gmail.com>
2024-10-22 00:22:49 +0900

    init: 개발환경 설정 (Tanstack Router)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-// https://vitejs.dev/config/
+import viteReact from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), viteReact()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, './src') },
