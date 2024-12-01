63635d4bbfc4c7979d8d7d125d2efdef3ae8134d
Jaewoong Hwang <noa9925@gmail.com>
2024-12-01 21:14:50 +0900

    chore: mdx 문서 파싱을 위한 의존성 설치

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import path from 'path'
+import mdx from '@mdx-js/rollup'
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
+  plugins: [
+    TanStackRouterVite(),
+    viteReact(),
+    {
+      enforce: 'pre',
+      ...mdx({
+        /* jsxImportSource: …, otherOptions… */
+      })
+    }
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, './src') },
