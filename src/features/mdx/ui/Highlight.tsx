60cb71311e9b43616bbc10fc3c2d0e9483ab025e
Jaewoong Hwang <noa9925@gmail.com>
2024-12-16 23:09:34 +0900

    feat: MDX 커스텀 컴포넌트 제작 (HightLight)

--- src/features/mdx/ui/Highlight.tsx
+++ src/features/mdx/ui/Highlight.tsx
@@ -1,15 +1,34 @@
 export interface HighlightProps {
   children: React.ReactNode
-  color?: 'yellow' | 'green' | 'blue' | 'pink'
+  color?:
+    | 'code'
+    | 'gray'
+    | 'brown'
+    | 'orange'
+    | 'yellow'
+    | 'green'
+    | 'blue'
+    | 'purple'
+    | 'red'
 }
 
 export const Highlight = ({ children, color = 'yellow' }: HighlightProps) => {
   const colorMap = {
-    yellow: 'bg-yellow-200 dark:bg-yellow-900',
-    green: 'bg-green-200 dark:bg-green-900',
-    blue: 'bg-blue-200 dark:bg-blue-900',
-    pink: 'bg-pink-200 dark:bg-pink-900'
+    code: 'bg-slate-200 border-none text-slate-900',
+    gray: 'bg-[#F1F1EF] border-[#E3E2E0] text-[#8C8B89]',
+    brown: 'bg-[#F4EEEE] border-[#F1E4E0] text-[#A5745D]',
+    orange: 'bg-[#FBECDD] border-[#FADEC9] text-[#D9730E]',
+    yellow: 'bg-[#FCF3DB] border-[#FDECC8] text-[#CB912F]',
+    green: 'bg-[#EDF3ED] border-[#DBEDDB] text-[#4F8969]',
+    blue: 'bg-[#E7F3F8] border-[#D3E5EF] text-[#347EA9]',
+    purple: 'bg-[#F7F3F8] border-[#EDE5F2] text-[#9065B0]',
+    red: 'bg-[#FDEBEC] border-[#FFE2DD] text-[#D44C47]'
   }
 
-  return <span className={`rounded px-1 ${colorMap[color]}`}>{children}</span>
+  return (
+    <span
+      className={`rounded-md border px-2 py-1 text-lg font-bold ${colorMap[color]}`}>
+      {children}
+    </span>
+  )
 }
--- src/features/mdx/ui/MdxRenderer.tsx
+++ src/features/mdx/ui/MdxRenderer.tsx
@@ -18,6 +18,7 @@ type EmphasisProps = ComponentPropsWithoutRef<'em'>
 type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>
 type ListProps = ComponentPropsWithoutRef<'ul' | 'ol'>
 type ListItemProps = ComponentPropsWithoutRef<'li'>
+type AnchorProps = ComponentPropsWithoutRef<'a'>
 
 export const MdxRenderer = ({ content }: MdxRendererProps) => {
   const [Content, setContent] = useState<React.ComponentType | null>(null)
@@ -26,61 +27,73 @@ export const MdxRenderer = ({ content }: MdxRendererProps) => {
     () => ({
       h1: (props: HeadingProps) => (
         <h1
-          className="my-4 text-2xl font-bold text-[#23f323]"
+          className="my-4 text-3xl font-bold text-slate-900"
           {...props}
         />
       ),
       h2: (props: HeadingProps) => (
         <h2
-          className="my-3 text-xl font-semibold"
+          className="my-3 text-2xl font-semibold text-slate-900"
+          {...props}
+        />
+      ),
+      h3: (props: HeadingProps) => (
+        <h3
+          className="my-2 text-xl font-semibold text-slate-900"
           {...props}
         />
       ),
       p: (props: ParagraphProps) => (
         <p
-          className="my-2"
+          className="my-3 text-lg text-slate-900"
+          {...props}
+        />
+      ),
+      a: (props: AnchorProps) => (
+        <a
+          className="font-bold text-slate-900 hover:text-slate-600 hover:underline"
           {...props}
         />
       ),
       strong: (props: StrongProps) => (
         <strong
-          className="font-bold"
+          className="font-bold text-slate-900"
           {...props}
         />
       ),
       em: (props: EmphasisProps) => (
         <em
-          className="italic"
+          className="italic text-slate-900"
           {...props}
         />
       ),
       blockquote: (props: BlockquoteProps) => (
         <blockquote
-          className="my-4 border-l-4 border-gray-200 pl-4 italic"
+          className="pl-4 italic font-bold border-l-4 border-slate-900 text-slate-900"
           {...props}
         />
       ),
       ul: (props: ListProps) => (
         <ul
-          className="my-4 list-inside list-disc"
+          className="my-2 text-lg list-disc list-inside text-slate-900"
           {...props}
         />
       ),
       ol: (props: ListProps) => (
         <ol
-          className="my-4 list-inside list-decimal"
+          className="my-2 text-lg list-decimal list-inside text-slate-900"
           {...props}
         />
       ),
       li: (props: ListItemProps) => (
         <li
-          className="my-1"
+          className="my-2 text-lg text-slate-900"
           {...props}
         />
       ),
-      inlineCode: (props: ComponentPropsWithoutRef<'code'>) => (
+      code: (props: ComponentPropsWithoutRef<'code'>) => (
         <code
-          className="rounded bg-gray-100 px-1 py-0.5 font-mono text-sm"
+          className="rounded bg-slate-200 px-1 py-0.5 font-mono text-xl"
           {...props}
         />
       ),
@@ -97,14 +110,12 @@ export const MdxRenderer = ({ content }: MdxRendererProps) => {
           remarkPlugins: [remarkGfm],
           useMDXComponents: () => components
         })
-
         setContent(() => MDXContent)
       } catch (error) {
         console.error(error)
         setContent(() => () => <div>콘텐츠 렌더링 중 오류가 발생했습니다.</div>)
       }
     }
-
     loadContent()
   }, [components, content])
 
